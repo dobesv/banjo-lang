03133023c7e5096f78750190c9682108b2e346c9
Dobes Vandermeer <dobesv@gmail.com>
2013-04-01 21:44:01 -0700

    Test case for referring to object fields using a quoted string literal.

--- test/banjo/parser/ast/TestProjectionParser.java
+++ test/banjo/parser/ast/TestProjectionParser.java
@@ -16,6 +16,9 @@ public class TestProjectionParser {
 	@Test public void specialCharField1() { test("a.\\-\\-", FieldRef.class); }
 	@Test public void specialCharField2() { test("a.\\.\\.", FieldRef.class); }
 	@Test public void specialCharField3() { test("a.\\.", "a.\\.", FieldRef.class); }
+	@Test public void specialCharField1q() { test("a.\"--\"", "a.\\-\\-", FieldRef.class); }
+	@Test public void specialCharField2q() { test("a.\"..\"", "a.\\.\\.", FieldRef.class); }
+	@Test public void specialCharField3q() { test("a.\".\"", "a.\\.", FieldRef.class); }
 
 	
 	private void test(String source, Class<? extends Expr> expectedClass) {
