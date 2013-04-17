38e454fa3c1b543154400c789c4eafd2d8d341cc
Dobes Vandermeer <dobesv@gmail.com>
2013-04-16 23:06:06 -0700

    Add an empty test here so the ant build succeeds.

--- test/banjo/idesupport/test/TestSourceFileAnalysis.java
+++ test/banjo/idesupport/test/TestSourceFileAnalysis.java
@@ -1,33 +1,11 @@
 package banjo.idesupport.test;
 
-import static banjo.dom.test.ParseTestUtils.test;
-import static org.junit.Assert.assertEquals;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
+import org.junit.Ignore;
 import org.junit.Test;
-
-import banjo.dom.Comment;
-import banjo.dom.Ellipsis;
-import banjo.dom.Expr;
-import banjo.dom.HasFileRange;
-import banjo.dom.Identifier;
-import banjo.dom.NumberLiteral;
-import banjo.dom.ObjectLiteral;
-import banjo.dom.OperatorRef;
-import banjo.dom.StringLiteral;
-import banjo.dom.TokenVisitor;
-import banjo.dom.UnitRef;
-import banjo.dom.Whitespace;
-import banjo.idesupport.SourceFileAnalysis;
-import banjo.parser.BanjoParser;
-import banjo.parser.BanjoScanner;
-import banjo.parser.util.TokenCollector;
 
 public class TestSourceFileAnalysis {
 
-	
+	@Ignore
+	@Test
+	public void noOp() { }
 }
