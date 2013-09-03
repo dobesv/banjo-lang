33023dbdd2d2366112e6b1c4ba54a427a0e60210
Dobes Vandermeer <dobesv@gmail.com>
2013-09-02 22:55:39 -0700

    Test that single-arg lambdas can use a pattern without wrapping in parentheses.

--- test/banjo/dom/test/TestArgPatterns.java
+++ test/banjo/dom/test/TestArgPatterns.java
@@ -14,14 +14,20 @@ public class TestArgPatterns {
 	@Test public void testUnpackObject5() { test("({x=y}) -> y", "{(__g_obj3) = {(y) = y}(__g_obj3.x)}"); }
 	@Test public void testUnpackObject6() { test("({x={y}}) -> y", "{(__g_obj3) = {(__g_obj3) = {(y) = y}(__g_obj3.y)}(__g_obj3.x)}"); }
 	@Test public void testUnpackObject7() { test("({x=[y]}) -> y", "{(__g_obj3) = {(__g_lst3) = {(y) = y}(__g_lst3[0])}(__g_obj3.x)}"); }
+	@Test public void testUnpackObject8() { test("{x} -> x", "{(__g_obj3) = {(x) = x}(__g_obj3.x)}"); }
+	@Test public void testUnpackObject9() { test("{x,y,z} -> z", "{(__g_obj3) = {(x, y, z) = z}(__g_obj3.x, __g_obj3.y, __g_obj3.z)}"); }
 
 	@Test public void testUnpackList1() { test("([y]) -> y", "{(__g_lst3) = {(y) = y}(__g_lst3[0])}"); }
 	@Test public void testUnpackList2() { test("([x,y,z]) -> z", "{(__g_lst3) = {(x, y, z) = z}(__g_lst3[0], __g_lst3[1], __g_lst3[2])}"); }
 	@Test public void testUnpackList3() { test("([x,{z}]) -> z", "{(__g_lst3) = {(x, __g_obj5) = {(z) = z}(__g_obj5.z)}(__g_lst3[0], __g_lst3[1])}"); }
+	@Test public void testUnpackList4() { test("[y] -> y", "{(__g_lst3) = {(y) = y}(__g_lst3[0])}"); }
+	@Test public void testUnpackList5() { test("[x,y,z] -> z", "{(__g_lst3) = {(x, y, z) = z}(__g_lst3[0], __g_lst3[1], __g_lst3[2])}"); }
 
 	@Test public void testUnpackLazy1() { test("(-> z) -> z", "{(__g_lazy3) = {(z) = z}(__g_lazy3())}"); }
 	@Test public void testUnpackLazy2() { test("(-> {z}) -> z", "{(__g_lazy3) = {(__g_obj5) = {(z) = z}(__g_obj5.z)}(__g_lazy3())}"); }
 	@Test public void testUnpackLazy3() { test("(-> [z]) -> z", "{(__g_lazy3) = {(__g_lst5) = {(z) = z}(__g_lst5[0])}(__g_lazy3())}"); }
+	@Test public void testUnpackLazy4() { test("{(true && -> x) = x}", "{true.\\&\\&(__g_lazy4) = {(x) = x}(__g_lazy4())}"); }
+	@Test public void testUnpackLazy5() { test("{(true && (-> x)) = x}", "{true.\\&\\&(__g_lazy4) = {(x) = x}(__g_lazy4())}"); }
 
 
 }
