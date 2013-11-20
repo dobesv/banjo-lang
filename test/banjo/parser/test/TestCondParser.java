1445d9d4db833d17eb32963b5e3a84dd3fe8bcf5
Dobes Vandermeer <dobesv@gmail.com>
2013-11-19 22:36:01 -0800

    Fix unit test to expect the appropriate class here.

--- test/banjo/parser/test/TestCondParser.java
+++ test/banjo/parser/test/TestCondParser.java
@@ -2,8 +2,9 @@ package banjo.parser.test;
 
 import org.junit.Test;
 
-import banjo.dom.BadExpr;
+import banjo.dom.Expr;
 import banjo.dom.core.Call;
+import banjo.dom.core.ObjectLiteral;
 
 public class TestCondParser {
 
@@ -14,7 +15,8 @@ public class TestCondParser {
 	@Test public void testCond5() { testParseCond("   a=>b\n   c=>d\n   e=>f\n   h", "a.\\=\\>({() = b}).\\;({() = c.\\=\\>({() = d}).\\;({() = e.\\=\\>({() = f}).\\;({() = h})})})"); }
 	@Test public void testCond6() { testParseCond("a < 0 => -a\na", "a.\\<\\=\\>(0).\\<.\\=\\>({() = a.\\-}).\\;({() = a})"); }
 	@Test public void testCond7() { testParseCond("a=>b\nc", "a.\\=\\>({() = b}).\\;({() = c})"); }
-	@Test public void testCond8() { testParseCond("{(a + e) =\n  a => b.c(d)\n  e\n}", "{(a + e) = a.\\=\\>({() = b.c(d)}).\\;({() = e})}"); }
+
+	@Test public void testCondInMethod1() { testParseCond("{(a + e) =\n  a => b.c(d)\n  e\n}", "{(a + e) = a.\\=\\>({() = b.c(d)}).\\;({() = e})}", ObjectLiteral.class); }
 
 	@Test public void testLazyOr() { testParseCond("a || b", "a.\\|\\|({() = b})"); }
 	@Test public void testLazyAnd() { testParseCond("a && b", "a.\\&\\&({() = b})"); }
@@ -24,8 +26,7 @@ public class TestCondParser {
 	public static void testParseCond(String source, String expectedSource) {
 		ParseTestUtils.test(source, 0, null, Call.class, expectedSource);
 	}
-	private void testParseCond(String source, Class<? extends BadExpr> expectedError, int expectedErrorCount) {
-		ParseTestUtils.test(source, expectedErrorCount, expectedError, Call.class, null);
+	public static void testParseCond(String source, String expectedSource, Class<? extends Expr> expectedClass) {
+		ParseTestUtils.test(source, 0, null, expectedClass, expectedSource);
 	}
-
 }
