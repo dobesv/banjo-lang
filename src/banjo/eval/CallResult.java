0b821915b748f978ccbb9f6e53d2dbc8e961926e
Dobes Vandermeer <dobesv@gmail.com>
2015-01-04 14:33:04 -0800

    Remove eclipse metadata from git

--- src/banjo/eval/CallResult.java
+++
@@ -1,5 +0,0 @@
-package banjo.eval;
-
-public interface CallResult {
-	public <T> T acceptVisitor(CallResultVisitor<T> visitor);
-}
--- src/banjo/eval/ContractFailure.java
+++
@@ -1,9 +0,0 @@
-package banjo.eval;
-
-public class ContractFailure extends Error {
-
-	public ContractFailure(String string) {
-		super(string);
-	}
-
-}
