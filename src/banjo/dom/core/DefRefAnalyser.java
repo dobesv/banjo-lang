443b2aecc87a75ca6edbe3e563044a119f4d2d27
Dobes Vandermeer <dobesv@gmail.com>
2015-02-13 14:19:54 -0800

    Update the def/ref analyzer to track slots - although they aren't resolved because we need type information for that, this can at least help identify slot references that have absolutely no corresponding slot def available.

--- src/banjo/dom/core/DefRefAnalyser.java
+++ src/banjo/dom/core/DefRefAnalyser.java
@@ -6,47 +6,48 @@ import fj.Ord;
 import fj.P;
 import fj.P2;
 import fj.data.List;
+import fj.data.Option;
 import fj.data.TreeMap;
 
 public class DefRefAnalyser implements CoreExprAlgebra<DefRefAnalyser> {
-	public static final TreeMap<Identifier,List<Identifier>> EMPTY_REF_MAP = TreeMap.empty(Identifier.ORD);
-	public static final TreeMap<String,List<Identifier>> EMPTY_DEF_MAP = TreeMap.empty(Ord.stringOrd);
+	public static final List<P2<Identifier,Identifier>> EMPTY_LOCAL_REFS = List.nil();
+	public static final List<Identifier> EMPTY_IDENTIFIER_LIST = List.nil();
 	public static final DefRefAnalyser EMPTY = new DefRefAnalyser();
-	List<Identifier> freeVars;
-	TreeMap<Identifier,List<Identifier>> refs;
-	TreeMap<String,List<Identifier>> defs;
+	List<Identifier> unresolvedLocalRefs;
+	List<P2<Identifier,Identifier>> localRefs;
+	List<Identifier> localDefs;
+	List<Identifier> slotRefs;
+	List<Identifier> slotDefs;
 
-	public DefRefAnalyser(List<Identifier> freeVars,
-            TreeMap<Identifier, List<Identifier>> refs,
-            TreeMap<String,List<Identifier>> defs) {
+	public DefRefAnalyser(List<Identifier> unresolvedLocalRefs,
+            List<P2<Identifier, Identifier>> refs,
+            List<Identifier> defs,
+            List<Identifier> slotRefs,
+            List<Identifier> slotDefs) {
 	    super();
-	    this.freeVars = freeVars;
-	    this.refs = refs;
-	    this.defs = defs;
+	    this.unresolvedLocalRefs = unresolvedLocalRefs;
+	    this.localRefs = refs;
+	    this.localDefs = defs;
+	    this.slotRefs = slotRefs;
+	    this.slotDefs = slotDefs;
     }
 
 	public DefRefAnalyser() {
-		this(List.nil(), EMPTY_REF_MAP, EMPTY_DEF_MAP);
+		this(List.nil(), EMPTY_LOCAL_REFS, EMPTY_IDENTIFIER_LIST, EMPTY_IDENTIFIER_LIST, EMPTY_IDENTIFIER_LIST);
     }
 
-	public static DefRefAnalyser union(DefRefAnalyser a, DefRefAnalyser b) {
-		TreeMap<Identifier,List<Identifier>> refs = a.refs;
-		for(P2<Identifier, List<Identifier>> p : b.refs) {
-			refs = refs.set(p._1(), refs.get(p._1()).orSome(List.nil()).append(p._2()));
-		}
-		TreeMap<String,List<Identifier>> defs = a.defs;
-		for(P2<String,List<Identifier>> p : b.defs) {
-			defs = defs.set(p._1(), defs.get(p._1()).orSome(List.nil()).append(p._2()));
-		}
+	public static DefRefAnalyser append(DefRefAnalyser a, DefRefAnalyser b) {
 		return new DefRefAnalyser(
-				a.freeVars.append(b.freeVars),
-				refs,
-				defs
+				a.unresolvedLocalRefs.append(b.unresolvedLocalRefs),
+				a.localRefs.append(b.localRefs),
+				a.localDefs.append(b.localDefs),
+				a.slotRefs.append(b.slotRefs),
+				a.slotDefs.append(b.slotDefs)
 		);
 	}
 
 	public static DefRefAnalyser unionList(List<DefRefAnalyser> maps) {
-		return maps.foldLeft(DefRefAnalyser::union, EMPTY);
+		return maps.foldLeft(DefRefAnalyser::append, EMPTY);
 	}
 
 	@Override
@@ -57,7 +58,9 @@ public class DefRefAnalyser implements CoreExprAlgebra<DefRefAnalyser> {
 	@Override
     public DefRefAnalyser objectLiteral(List<SourceFileRange> ranges,
             List<P2<Identifier, DefRefAnalyser>> slots) {
-	    return unionList(slots.map(P2.__2()));
+	    DefRefAnalyser x = unionList(slots.map(P2.__2()));
+	    List<Identifier> newSlotDefs = x.slotDefs.append(slots.map(P2.__1()));
+	    return new DefRefAnalyser(x.unresolvedLocalRefs, x.localRefs, x.localDefs, x.slotRefs, newSlotDefs);
     }
 
 	@Override
@@ -80,13 +83,13 @@ public class DefRefAnalyser implements CoreExprAlgebra<DefRefAnalyser> {
 	@Override
     public DefRefAnalyser call(List<SourceFileRange> ranges,
             DefRefAnalyser function, List<DefRefAnalyser> args) {
-	    return union(function, unionList(args));
+	    return append(function, unionList(args));
     }
 
 	@Override
     public DefRefAnalyser extend(List<SourceFileRange> ranges,
             DefRefAnalyser base, DefRefAnalyser extension) {
-	    return union(base, extension);
+	    return append(base, extension);
     }
 
 	@Override
@@ -96,27 +99,26 @@ public class DefRefAnalyser implements CoreExprAlgebra<DefRefAnalyser> {
 
 	@Override
     public DefRefAnalyser identifier(List<SourceFileRange> ranges, String id) {
-	    return new DefRefAnalyser(List.single(new Identifier(ranges, id)), EMPTY_REF_MAP, EMPTY_DEF_MAP);
+	    return new DefRefAnalyser(List.single(new Identifier(ranges, id)), EMPTY_LOCAL_REFS, EMPTY_IDENTIFIER_LIST, EMPTY_IDENTIFIER_LIST, EMPTY_IDENTIFIER_LIST);
     }
 
 	@Override
     public DefRefAnalyser let(List<SourceFileRange> ranges,
             List<P2<Identifier, DefRefAnalyser>> bindings, DefRefAnalyser body) {
-		DefRefAnalyser t = union(unionList(bindings.map(P2.__2())), body);
+		DefRefAnalyser t = append(unionList(bindings.map(P2.__2())), body);
 		return t.defs(bindings.map(P2.__1()));
     }
 
 	private DefRefAnalyser defs(List<Identifier> newNames) {
-	    TreeMap<Identifier, List<Identifier>> newRefs = TreeMap.treeMap(
-				Identifier.ORD,
-				newNames.map(id -> P.p(id, this.refs.get(id).orSome(List.nil()).append(this.freeVars.filter(v -> id.compareTo(v) == 0))))
+		TreeMap<String, Identifier> bindings = TreeMap.treeMap(Ord.stringOrd, newNames.map(name -> P.p(name.id, name)));
+
+
+	    List<P2<Identifier, Identifier>> newRefs = this.localRefs.append(
+	    		Option.somes(this.unresolvedLocalRefs.map(ref -> bindings.get(ref.id).map(def -> P.p(ref, def))))
 		);
-		List<Identifier> newFreeVars = this.freeVars.filter(k -> ! newRefs.contains(k));
-		TreeMap<String, List<Identifier>> newDefs = TreeMap.treeMap(
-				Ord.stringOrd,
-				newNames.map(id -> P.p(id.id, this.defs.get(id.id).orSome(List.nil()).snoc(id)))
-		);
-	    return new DefRefAnalyser(newFreeVars, newRefs.union(this.refs), newDefs.union(this.defs));
+		List<Identifier> newFreeVars = this.unresolvedLocalRefs.filter(name -> ! bindings.contains(name.id));
+		List<Identifier> newDefs = this.localDefs.append(newNames);
+	    return new DefRefAnalyser(newFreeVars, newRefs, newDefs, this.slotRefs, this.slotRefs);
     }
 
 	@Override
@@ -127,7 +129,8 @@ public class DefRefAnalyser implements CoreExprAlgebra<DefRefAnalyser> {
 
 	@Override
     public DefRefAnalyser slotReference(List<SourceFileRange> ranges, DefRefAnalyser object, Identifier slotName) {
-	    return object;
+		List<Identifier> slotRefs = object.slotRefs.snoc(slotName);
+		return new DefRefAnalyser(object.unresolvedLocalRefs, object.localRefs, object.localDefs, slotRefs, object.slotDefs);
     }
 
 
