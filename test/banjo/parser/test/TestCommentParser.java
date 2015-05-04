8e33463cd1b102af27c83791f7b40cd42f5e2492
Dobes Vandermeer <dobesv@gmail.com>
2015-05-04 16:24:24 -0700

    Update comment parsing test to work with '#' for comments instead of ';'

--- test/banjo/parser/test/TestCommentParser.java
+++ test/banjo/parser/test/TestCommentParser.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 
 
 public class TestCommentParser {
-	@Test public void testSl1() { test(";comment\na", "a"); }
-	@Test public void testSl2() { test("a;comment\n", "a"); }
-	@Test public void testSl3() { test(";\na;comment\n", "a"); }
-	@Test public void testSl4() { test("\n; \n a ; comment \n\n", "a"); }
-	@Test public void testSl5() { test(" ; \n\na", "a"); }
+	@Test public void testSl1() { test("#comment\na", "a"); }
+	@Test public void testSl2() { test("a#comment\n", "a"); }
+	@Test public void testSl3() { test("#\na#comment\n", "a"); }
+	@Test public void testSl4() { test("\n# \n a # comment \n\n", "a"); }
+	@Test public void testSl5() { test(" # \n\na", "a"); }
 }
