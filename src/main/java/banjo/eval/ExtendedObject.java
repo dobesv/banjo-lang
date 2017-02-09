d34e45963d4013303c00826d6cec52006251c354
Dobes Vandermeer <dobesv@gmail.com>
2017-02-08 20:23:14 -0800

    Some little naming tweaks

--- src/main/java/banjo/eval/ExtendedObject.java
+++ src/main/java/banjo/eval/ExtendedObject.java
@@ -13,20 +13,20 @@ import fj.data.Set;
 public class ExtendedObject extends ValueToStringTrait implements Value {
     public static final class ChainedBaseFunctionImpl extends ValueToStringTrait implements Value {
 	    public final Value base;
-	    public final Value prevImpl;
+	    public final Value baseCallable;
 
-		public ChainedBaseFunctionImpl(Value base, Value prevImpl) {
+		public ChainedBaseFunctionImpl(Value base, Value baseCallable) {
 	    	this.base = base;
-	    	this.prevImpl = prevImpl;
+	    	this.baseCallable = baseCallable;
         }
 
 		@Override
-	    public Value call(List<Value> trace, Value recurse, Value newPrevImpl, List<Value> arguments) {
-	    	return base.call(trace, recurse, chainBaseImpl(prevImpl, newPrevImpl), arguments);
+	    public Value call(List<Value> trace, Value callee, Value newBaseCallable, List<Value> arguments) {
+	    	return base.call(trace, callee, chainBaseImpl(baseCallable, newBaseCallable), arguments);
 	    }
 		
 		public ChainedBaseFunctionImpl update(Value newBase, Value newPrevImpl) {
-			return (newBase == base && newPrevImpl == prevImpl) ? this : new ChainedBaseFunctionImpl(newBase, newPrevImpl);
+			return (newBase == base && newPrevImpl == baseCallable) ? this : new ChainedBaseFunctionImpl(newBase, newPrevImpl);
 		}
 		
         @Override
